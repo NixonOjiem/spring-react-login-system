24083af5b0ce277572388d5eacd0d61034a7c7c7
Nick <nicksonogm@gmail.com>
2025-11-14 10:53:36 +0300

    created DTO for login and signup

--- backend/src/main/java/com/nickogm/backend/payload/request/LoginRequest.java
+++ backend/src/main/java/com/nickogm/backend/payload/request/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.nickogm.backend.payload.request;
 
 public class LoginRequest {
-
+    private String username;
+    private String password;
 }
--- backend/src/main/java/com/nickogm/backend/payload/request/SignupRequest.java
+++ backend/src/main/java/com/nickogm/backend/payload/request/SignupRequest.java
@@ -1,5 +1,7 @@
 package com.nickogm.backend.payload.request;
 
 public class SignupRequest {
-
+    private String username;
+    private String email;
+    private String password;
 }
