277965d377760830fbc430d9e8131828980415fa
Nick <nicksonogm@gmail.com>
2025-11-14 09:49:04 +0300

    added dependencies to connect to mysql and issue JWT tokens with AUth

---
+++ backend/src/main/java/com/nickogm/backend/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.nickogm.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "Welcome to NickOGM backend";
+    }
+}
